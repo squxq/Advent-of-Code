5c45cb61c5db9b58ff58c74ddceb567a19d93512
francisco <franciscotomas0430@gmail.com>
2023-12-19 15:45:41 +0000

    fix: day 08 redundant code

--- days/08/index.ts
+++ days/08/index.ts
@@ -1,6 +1,63 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as fs from "fs";
 
+class Helpers {
+  /** return the least commom multiple in the given number array, set
+   * ASSUME: set.length >= 1
+   * @param {number[]} set
+   * @returns {number}
+   */
+  public lcm(set: number[]): number {
+    let ans: number = set[0]!;
+
+    for (let index: number = 1; index < set.length; index++) {
+      ans = ans * (set[index]! / this.gcd(ans, set[index] as number));
+    }
+
+    return ans;
+  }
+
+  /** create object network with given lines
+   * @public
+   * @param {string[]} lines
+   * @returns {Record<string, { L: string; R: string }>}
+   */
+  public createNetwork(
+    lines: string[],
+    callback?: (node: string) => void,
+  ): Record<string, { L: string; R: string }> {
+    const network: Record<string, { L: string; R: string }> = {};
+
+    for (const line of lines) {
+      const [node, nextElements] = line.split(" = ") as [string, string];
+      const [left, right] = nextElements
+        .substring(1, nextElements.length - 1)
+        .split(", ") as [string, string];
+
+      network[node] = { L: left, R: right };
+
+      if (callback !== undefined) callback(node);
+    }
+
+    return network;
+  }
+
+  /** return the greatest common divisor between given numbers, a & b
+   * @param {number} a
+   * @param {number} b
+   * @returns {number}
+   */
+  private gcd(a: number, b: number): number {
+    while (b !== 0) {
+      const temp: number = b;
+      b = a % b;
+      a = temp;
+    }
+
+    return a;
+  }
+}
+
 export function part1(input: string): number {
   let ans: number = 0;
 
@@ -8,16 +65,10 @@ export function part1(input: string): number {
   lines.splice(1, 1);
 
   const directions: string = lines.shift()!;
+  const helpers: Helpers = new Helpers();
 
-  const network: Record<string, { L: string; R: string }> = {};
-  for (const line of lines) {
-    const [node, nextElements] = line.split(" = ");
-    const [left, right] = nextElements!
-      .substring(1, nextElements!.length - 1)
-      .split(", ");
-
-    network[node!] = { L: left!, R: right! };
-  }
+  const network: Record<string, { L: string; R: string }> =
+    helpers.createNetwork(lines);
 
   let current: string = "AAA";
   let direction: number = 0;
@@ -34,55 +85,19 @@ export function part1(input: string): number {
   return ans;
 }
 
-/** return the greatest common divisor between given numbers, a & b
- * @param {number} a
- * @param {number} b
- * @returns {number}
- */
-function gcd(a: number, b: number): number {
-  while (b !== 0) {
-    const temp: number = b;
-    b = a % b;
-    a = temp;
-  }
-
-  return a;
-}
-
-/** return the least commom multiple in the given number array, set
- * ASSUME: set.length >= 1
- * @param {number[]} set
- * @returns  number
- */
-function lcm(set: number[]): number {
-  let ans: number = set[0]!;
-
-  for (let index: number = 1; index < set.length; index++) {
-    ans = ans * (set[index]! / gcd(ans, set[index] as number));
-  }
-
-  return ans;
-}
-
 export function part2(input: string): number {
   const lines: string[] = input.trim().split("\n");
   lines.splice(1, 1);
 
   const directions: string = lines.shift()!;
-
-  const network: Record<string, { L: string; R: string }> = {};
   const nodes: string[] = [];
 
-  for (const line of lines) {
-    const [node, nextElements] = line.split(" = ") as [string, string];
-    const [left, right] = nextElements
-      .substring(1, nextElements.length - 1)
-      .split(", ") as [string, string];
+  const helpers: Helpers = new Helpers();
 
-    network[node] = { L: left, R: right };
-
-    if (node[node.length - 1] === "A") nodes.push(node);
-  }
+  const network: Record<string, { L: string; R: string }> =
+    helpers.createNetwork(lines, (node: string) => {
+      if (node[node.length - 1] === "A") nodes.push(node);
+    });
 
   let direction: number = 0;
   let iterations: number = 0;
@@ -111,7 +126,7 @@ export function part2(input: string): number {
     direction++;
   }
 
-  return lcm(cycles);
+  return helpers.lcm(cycles);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
