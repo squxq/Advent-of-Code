b830bbb50a6eff4c2ad08946e134ef4da2bb0473
francisco <franciscotomas0430@gmail.com>
2023-12-19 13:28:30 +0000

    fix: day 03 redundant code

--- days/03/index.ts
+++ days/03/index.ts
@@ -1,52 +1,125 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as fs from "fs";
 
-function isSymbol(char: string | undefined): boolean {
-  if (char === undefined) {
-    return false;
-  }
-  return /[^0-9.]/.test(char);
-}
-
-function adjacentSymbol(
+/**
+ * @callback Callback
+ * @param {number} start
+ * @param {number} col
+ * @param {number} row
+ * @param {string} partNumber
+ * @returns {void}
+ */
+type Callback = (
   start: number,
-  end: number,
+  col: number,
   row: number,
-  matrix: string[][],
-): boolean {
-  if (isSymbol(matrix[row]![start - 1]) || isSymbol(matrix[row]![end + 1])) {
-    return true;
+  partNumber: string,
+) => void;
+
+class Helpers {
+  /** return true if given char is a symbol - not a digit nor a "."
+   * @param {string | undefined} char
+   * @returns {boolean}
+   */
+  private isSymbol(char: string | undefined): boolean {
+    if (char === undefined) {
+      return false;
+    }
+    return /[^0-9.]/.test(char);
   }
 
-  for (let c: number = start - 1; c <= end + 1; c++) {
+  /** return true there is a symbol (previously defined) in any of the below indexes; otherwise return false:
+   * - matrix[row - 1][start - 1] -> matrix[row - 1][end + 1]
+   * - matrix[row][start - 1] & matrix[row][end + 1]
+   * - matrix[row + 1][start - 1] -> matrix[row + 1][end + 1]
+   * @param {number} start
+   * @param {number} end
+   * @param {number} row
+   * @param {string[][]} matrix
+   * @returns {boolean}
+   */
+  public adjacentSymbol(
+    start: number,
+    end: number,
+    row: number,
+    matrix: string[][],
+  ): boolean {
     if (
-      (matrix[row - 1] !== undefined && isSymbol(matrix[row - 1]![c])) ||
-      (matrix[row + 1] !== undefined && isSymbol(matrix[row + 1]![c]))
+      this.isSymbol(matrix[row]![start - 1]) ||
+      this.isSymbol(matrix[row]![end + 1])
     ) {
       return true;
     }
+
+    for (let c: number = start - 1; c <= end + 1; c++) {
+      if (
+        (matrix[row - 1] !== undefined && this.isSymbol(matrix[row - 1]![c])) ||
+        (matrix[row + 1] !== undefined && this.isSymbol(matrix[row + 1]![c]))
+      ) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
-  return false;
-}
+  /** return all the positions, if they exist, of "*" in any of the previously defined indexes
+   * @param {number} start
+   * @param {number} end
+   * @param {number} row
+   * @param {string[][]} matrix
+   * @returns {Array<{ col: number; row: number }>}
+   */
+  public adjacentStar(
+    start: number,
+    end: number,
+    row: number,
+    matrix: string[][],
+  ): Array<{ col: number; row: number }> {
+    const result: Array<{ col: number; row: number }> = [];
 
-function adjacentStar(
-  start: number,
-  end: number,
-  row: number,
-  matrix: string[][],
-): Array<{ col: number; row: number }> {
-  const result: Array<{ col: number; row: number }> = [];
+    matrix[row]![start - 1] === "*" && result.push({ col: start - 1, row });
+    matrix[row]![end + 1] === "*" && result.push({ col: end + 1, row });
 
-  matrix[row]![start - 1] === "*" && result.push({ col: start - 1, row });
-  matrix[row]![end + 1] === "*" && result.push({ col: end + 1, row });
+    for (let col: number = start - 1; col <= end + 1; col++) {
+      matrix[row - 1]?.[col] === "*" && result.push({ col, row: row - 1 });
+      matrix[row + 1]?.[col] === "*" && result.push({ col, row: row + 1 });
+    }
 
-  for (let col: number = start - 1; col <= end + 1; col++) {
-    matrix[row - 1]?.[col] === "*" && result.push({ col, row: row - 1 });
-    matrix[row + 1]?.[col] === "*" && result.push({ col, row: row + 1 });
+    return result;
   }
 
-  return result;
+  /** given a function, callback, and a matrix, traverse the matrix and produce callback when a number is found
+   * @param {string[][]} matrix
+   * @param {Callback} callback
+   * @returns {void}
+   */
+  public traverseMatrix(matrix: string[][], callback: Callback): void {
+    const height: number = matrix.length;
+    const width: number = matrix[0]!.length;
+
+    for (let row: number = 0; row < height; row++) {
+      let partNumber: string = "";
+      let start: number = 0;
+
+      for (let col: number = 0; col <= width; col++) {
+        const char: string =
+          col !== width ? (matrix[row]![col] as string) : ".";
+
+        if (/^\d$/.test(char)) {
+          if (partNumber.length === 0) {
+            start = col;
+          }
+          partNumber += char;
+        } else if (partNumber.length > 0) {
+          callback(start, col, row, partNumber);
+
+          partNumber = "";
+          start = 0;
+        }
+      }
+    }
+  }
 }
 
 export function part1(input: string): number {
@@ -56,30 +129,14 @@ export function part1(input: string): number {
     .trim()
     .split("\n")
     .map((line) => line.split(""));
-  const height: number = matrix.length;
-  const width: number = matrix[0]!.length;
 
-  for (let row: number = 0; row < height; row++) {
-    let partNumber: string = "";
-    let start: number = 0;
+  const helpers: Helpers = new Helpers();
 
-    for (let col: number = 0; col <= width; col++) {
-      const char: string = col !== width ? matrix[row]![col]! : ".";
-
-      if (/^\d$/.test(char)) {
-        if (partNumber.length === 0) {
-          start = col;
-        }
-        partNumber += char;
-      } else if (partNumber.length > 0) {
-        if (adjacentSymbol(start, col - 1, row, matrix)) {
-          ans += Number(partNumber);
-        }
-        partNumber = "";
-        start = 0;
-      }
+  helpers.traverseMatrix(matrix, (start, col, row, partNumber) => {
+    if (helpers.adjacentSymbol(start, col - 1, row, matrix)) {
+      ans += Number(partNumber);
     }
-  }
+  });
 
   return ans;
 }
@@ -91,44 +148,27 @@ export function part2(input: string): number {
     .trim()
     .split("\n")
     .map((line) => line.split(""));
-  const height: number = matrix.length;
-  const width: number = matrix[0]!.length;
 
-  for (let row: number = 0; row < height; row++) {
-    let partNumber: string = "";
-    let start: number = 0;
+  const helpers: Helpers = new Helpers();
 
-    for (let col: number = 0; col <= width; col++) {
-      const char: string = col !== width ? matrix[row]![col]! : ".";
+  helpers.traverseMatrix(matrix, (start, col, row, partNumber) => {
+    const stars: Array<{ col: number; row: number }> = helpers.adjacentStar(
+      start,
+      col - 1,
+      row,
+      matrix,
+    );
 
-      if (/^\d$/.test(char)) {
-        if (partNumber.length === 0) {
-          start = col;
-        }
-        partNumber += char;
-      } else if (partNumber.length > 0) {
-        const stars: Array<{ col: number; row: number }> = adjacentStar(
-          start,
-          col - 1,
-          row,
-          matrix,
-        );
+    stars.forEach(({ col, row }: { col: number; row: number }) => {
+      const key: string = `${col},${row}`;
 
-        stars.forEach(({ col, row }: { col: number; row: number }) => {
-          const key: string = `${col},${row}`;
-
-          if (adjacentStars[key] !== undefined) {
-            adjacentStars[key]!.push(Number(partNumber));
-          } else {
-            adjacentStars[key] = [Number(partNumber)];
-          }
-        });
-
-        partNumber = "";
-        start = 0;
+      if (adjacentStars[key] !== undefined) {
+        adjacentStars[key]!.push(Number(partNumber));
+      } else {
+        adjacentStars[key] = [Number(partNumber)];
       }
-    }
-  }
+    });
+  });
 
   return Object.entries(adjacentStars).reduce(
     (acc: number, [, value]: [string, number[]]) => {
